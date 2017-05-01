b5ebbb7390c26c6e434529010449b0ccc7f59602
juliusb <juliusb>
2017-05-01 22:17:25 +0800

    more update

--- script2.js
+++ script2.js
@@ -1,25 +1,44 @@
 (function(){
     
+    var app = angular.module("githubViewer",[]);
     
-    console.log("Hello");
-
-
-    var MainController = function($scope, $http){
+    var MainController = function($scope, $http, $interval){
 
         var onUserComplete = function(response){
             $scope.user = response.data;
+            $http.get("http://url.here").then(onUserComplete, onError);
+        };
+
+        var onRepos = function(response){
+            $scope.repos = response.data;
         }
 
         var onError = function(reason){
-            $scope.error = "Could not Fetch";
+            $scope.error = "Could not Fetch the data";
+        };
+
+        var decrementCountdown = function(){
+            $scope.countdown -= 1;
+            if($scope.countdown < 1){
+                $scope.search($scope.username);
+            }
         }
-        var promise = $http.get("http://url.here").then(onUserComplete, onError);
 
+        var startCountdown = function(){
+            $interval(decrementCountdown,1000,$scope.countdown);
+        };
+
+        $scope.search = function(username){
+            $http.get("https://api.github.com/users/" + username)
+                    .then(onUserComplete, onError);
+        }
+
+        $scope.username = "angular";
         $scope.message = "Hello, Angular!";
+        $scope.repoSortOrder = "-stargazers_count";
+        $scope.countdown = 5;
+        startCountdown();
     }
-
-
-    var app = angular.module("githubViewer",[]);
 
     app.controller("MainController", ["$scope","$http",MainController]);
 
